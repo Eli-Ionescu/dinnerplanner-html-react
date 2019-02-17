e2a029d1707352f23f0511a9d7c97200c96f9113
Ruxandra22 <ruxandra22.osman@gmail.com>
2019-02-17 13:40:13 +0100

    created the sidebar

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -39,21 +39,40 @@ class Sidebar extends Component {
 
   render() {
     return (
-      <div className="Sidebar">
-        <h3>This is the sidebar</h3>
-        <p>
-          People:
-          <input
-            type="number"
-            value={this.state.numberOfGuests}
-            onChange={this.onNumberOfGuestsChanged}
-          />
-          <br />
-          Total number of guests: {this.state.numberOfGuests}
-        </p>
-      </div>
+          <div className="col-md-3" id="sidebar">
+            <div id="row" id="sidebarHeader">
+              <h3 className="col-xs-4">My Dinner</h3>
+              <div id="button_collapse" className="hidden-sm-up col-xs-4">
+                <i className="fa fa-bars fa-2x" aria-hidden="true" data-toggle="collapse" data-target="#innerSidebar"></i>
+              </div>
+            </div>
+            <div className="hidden-xs sidebar" id="innerSidebar">
+              <label htmlFor="numberPeople" className="people">People: </label>
+              <input
+                  type="number"
+                  value={this.state.numberOfGuests}
+                  onChange={this.onNumberOfGuestsChanged}
+              />
+              <table className="table">
+                <thead>
+                <tr>
+                  <th scope="col">Dish Name</th>
+                  <th scope="col">Cost</th>
+                </tr>
+                </thead>
+                <tbody id="selectedDishTableBody">
+                </tbody>
+              </table>
+              <p id="totalPrice"></p>
+              <button align="middle" className="button" id="confirmDinner">
+                Confirm Dinner
+              </button>
+            </div>
+          </div>
     );
   }
 }
+
+
 
 export default Sidebar;
