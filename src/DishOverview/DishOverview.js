a7e1c5b539a050e06aa93456d33c7ea7fa8adf10
Eli-Ionescu <ionescu.e.f@gamil.com>
2019-02-17 17:58:12 +0100

    Merge branch 'master' of https://github.com/Eli-Ionescu/dinnerplanner-html-react split dish details view # Conflicts: #	src/Dishes/Dishes.js

--- src/DishOverview/DishOverview.js
+++ src/DishOverview/DishOverview.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import "./DishOverview.css";
 import {Link} from "react-router-dom";
 import DinnerListOverview from "../DinnerListOverview/DinnerListOverview";
 
