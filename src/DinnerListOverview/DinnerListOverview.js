a9bf7ab5bd93009c4cf6f0dae2c5a5bf98439e69
Ruxandra22 <ruxandra22.osman@gmail.com>
2019-02-19 17:21:29 +0100

    dinner overview page

--- src/DinnerListOverview/DinnerListOverview.js
+++ src/DinnerListOverview/DinnerListOverview.js
@@ -1,17 +1,30 @@
 import React, { Component } from "react";
+import modelInstance from "../data/DinnerModel";
 
 class DinnerListOverview extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            // status: "LOADING"
+            status: "LOADING"
         };
     }
 
     render() {
+
+        let selectedDishes = modelInstance.getSelectedDishes().map(dish => (
+            <a>
+                <img className="img-thumbnail" src={dish.image}/>
+                <div className="caption" id="captionOverview">
+                    <p>{dish.title} {dish.pricePerServing} SEK</p>
+                </div>
+            </a>
+        ));
+
         return (
             <div className="DinnerListOverview">
-                DishListOverview
+                <div className="col-md-3">
+                    {selectedDishes}
+                </div>
             </div>
         );
     }
