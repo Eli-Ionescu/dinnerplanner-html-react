014f336ae108fe213c705373b4fbf1acf9b3f0e5
Eli-Ionescu <ionescu.e.f@gamil.com>
2019-02-25 11:43:10 +0100

    local storage

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import Welcome from "./Welcome/Welcome";
+import modelInstance from "./data/DinnerModel";
+import SelectDish from "./SelectDish/SelectDish";
+import "./App.css";
+import DinnerOverview from "./DinnerOverview/DinnerOverview";
+import DishDetails from "./DishDetails/DishDetails";
+import Printout from "./Printout/Printout";
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: "Dinner Planner"
+        };
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title">{this.state.title}</h1>
+
+                    {/* We rendered different component based on the path */}
+                    <Route exact path="/" component={Welcome} />
+                    <Route
+                        path="/search"
+                        render={() => <SelectDish model={modelInstance} />}
+                    />
+                    <Route
+                        path="/dishOverview"
+                        render={() => <DinnerOverview model={modelInstance} />}
+                    />
+                    <Route
+                        path="/details/:id"
+                        render={(props) => <DishDetails {...props} model={modelInstance} />}
+                    />
+                    <Route
+                        path="/printout"
+                        render={() => <Printout model={modelInstance} />}
+                    />
+                </header>
+            </div>
+        );
+    }
+}
+
+export default App;
